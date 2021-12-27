5422a1c4c7ff5b7e1ce7e0d8e79a0f7c06326271
mixren <m.s.zuravlov@gmail.com>
2021-12-27 18:23:42 +0200

    remove unnecessary

--- src/main/scala/io/github/mixren/evoscalabootcampexoplanetmarket/ExoplanetmarketServer.scala
+++ src/main/scala/io/github/mixren/evoscalabootcampexoplanetmarket/ExoplanetmarketServer.scala
@@ -40,34 +40,4 @@ object ExoplanetmarketServer {
     ).drain
   }
 
-  /*def stream[F[_]: Async]: Stream[F, Nothing] = {
-    for {
-      client <- Stream.resource(EmberClientBuilder.default[F].build)
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        DefRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        DefRoutes.jokeRoutes[F](jokeAlg) <+>
-        ExoplanetmarketRoutes.fetchExoplanetsRoutes[F]
-      ).orNotFound
-
-      // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
-      exitCode <- Stream.resource(
-        EmberServerBuilder.default[F]
-          .withHost(ipv4"0.0.0.0")
-          .withPort(port"8080")
-          .withHttpApp(finalHttpApp)
-          .build >>
-        Resource.eval(Async[F].never)
-      )
-    } yield exitCode
-
-  }.drain*/
 }
