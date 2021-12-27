6e34f2583ecf721584b311c2fce04e1d4e6e4b05
mixren <m.s.zuravlov@gmail.com>
2021-12-27 18:10:36 +0200

    now reservation is released at the end

--- src/main/scala/io/github/mixren/evoscalabootcampexoplanetmarket/purchase/PurchaseService.scala
+++ src/main/scala/io/github/mixren/evoscalabootcampexoplanetmarket/purchase/PurchaseService.scala
@@ -2,8 +2,10 @@ package io.github.mixren.evoscalabootcampexoplanetmarket.purchase
 
 import cats.data.EitherT
 import cats.effect.Async
-import io.github.mixren.evoscalabootcampexoplanetmarket.purchase.domain.{Purchase, PurchasePrice, PurchaseSuccess, ExoOldNewCardRequest}
+import cats.implicits.catsSyntaxApply
+import io.github.mixren.evoscalabootcampexoplanetmarket.purchase.domain.{ExoOldNewCardRequest, Purchase, PurchasePrice, PurchaseSuccess}
 import io.github.mixren.evoscalabootcampexoplanetmarket.user.domain.UserName
+
 import java.time.Instant
 import scala.concurrent.duration.DurationInt
 
@@ -17,7 +19,7 @@ class PurchaseService[F[_]: Async](reservationService: ReservationServiceT[F],
                                    repo: PurchaseRepositoryT[F]) extends PurchaseServiceT[F] {
 
   override def makePurchase(trio: ExoOldNewCardRequest, username: UserName): F[Either[String, PurchaseSuccess]] = {
-    (for {
+    val purchaseT = for {
       _      <- EitherT(reservationService.verifyAndExtendReservation(trio.exoplanetName, username, 5.minute))
       _      <- EitherT(bankingService.makePayment(trio.card, BigDecimal(4.99), SomeId("XXX555PPP")))
       _      <- EitherT(repo.addPurchase(Purchase(trio.exoplanetName,
@@ -25,10 +27,12 @@ class PurchaseService[F[_]: Async](reservationService: ReservationServiceT[F],
         username,
         PurchasePrice(BigDecimal(4.99)),
         Instant.now().toEpochMilli)) )      // TODO db write by repo.addPurchase still can fail. Although, very not likely, but why not to consider some fallback
-      _      <- EitherT.right[String](reservationService.releaseReservation(trio.exoplanetName, username))   // TODO release it in any case! mb in Main
-    } yield PurchaseSuccess.of(username, trio.exoplanetName, trio.exoplanetNewName))
-      .value
-  }
+      _      <- EitherT.right[String](reservationService.releaseReservation(trio.exoplanetName, username))
+    } yield PurchaseSuccess.of(username, trio.exoplanetName, trio.exoplanetNewName)
 
+    // No matter what's the result of the purchase, reservation is released at the end.
+    // Discards the second result so it's gonna return "purchaseT.value
+    purchaseT.value <* reservationService.releaseReservation(trio.exoplanetName, username)
+  }
 
 }
